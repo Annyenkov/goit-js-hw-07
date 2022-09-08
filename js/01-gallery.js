89420c61706353e464a2e95e9ea30aeff19cce00
Viktor Annyenkov <annyenkov@gmail.com>
2022-09-08 17:13:23 +0300

    test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const cardContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+cardContainer.insertAdjacentHTML('beforeend', cardsMarkup)
 
+cardContainer.addEventListener('click', onCardContainerClick)
+
+function onCardContainerClick(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(evt.target.dataset.source);
+}
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link" onclick="return false"  href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+  })
+    .join('');
+}
 console.log(galleryItems);
