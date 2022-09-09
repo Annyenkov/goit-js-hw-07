9d4ff5f8439e0e117141912bca145f1dee230819
Viktor Annyenkov <annyenkov@gmail.com>
2022-09-09 19:29:04 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const cardContainer = document.querySelector('.gallery');
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
-cardContainer.insertAdjacentHTML('beforeend', cardsMarkup)
-
-cardContainer.addEventListener('click', onCardContainerClick)
+cardContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+cardContainer.addEventListener('click', onCardContainerClick);
 
 function onCardContainerClick(evt) {
+  const instance = basicLightbox.create(`
+		<img width="1400" height="900" src="${evt.target.dataset.source}">
+	`);
   if (evt.target.nodeName !== "IMG") {
     return;
   }
-  console.log(evt.target.dataset.source);
+  instance.show();
 }
 
 function createGalleryCardsMarkup(galleryItems) {
@@ -30,4 +33,3 @@ function createGalleryCardsMarkup(galleryItems) {
   })
     .join('');
 }
-console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const cardContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+cardContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+cardContainer.addEventListener('click', onCardContainerClick, {once : true});
 
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}" onclick="return false">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+  })
+    .join('');
+}
+
+function onCardContainerClick(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  };
+   console.dir(evt.target.alt)
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+
+  });
+};
 console.log(galleryItems);
